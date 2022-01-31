1fceef7744f8f3925146e65003c1150ddad6684b
Tino <tino.agency@gmail.com>
2022-01-31 13:15:01 +0200

    New Fonts New Hero

--- build/assets/js/50_global.js
+++ build/assets/js/50_global.js
@@ -14,19 +14,6 @@ document.addEventListener('DOMContentLoaded', function(){
             toggleActions: 'play none none none',
             once: true });
     });
-    const runStroke = new Swiper('.run-stroke', {
-        slidesPerView: 'auto',
-        spaceBetween: 80,
-        freeMode: true,
-        loop: true,
-        speed: 7000,
-        disableOnInteraction: false,
-        allowTouchMove: false,
-        autoplay: {
-            delay: 0,
-            disableOnInteraction: false,
-        }
-    });
 
     const clientSlider = new Swiper('.partner-slider', {
         slidesPerView: 'auto',
